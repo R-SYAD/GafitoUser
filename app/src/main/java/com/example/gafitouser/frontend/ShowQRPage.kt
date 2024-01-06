1dcb7600837b2f56d71c868386f0fc2ee39ff8d4
Muhammad Irsyadul Fikri <irsyadulfikri270418@gmail.com>
2024-01-06 19:14:38 +0700

    QR + Map anjayyy

---
+++ app/src/main/java/com/example/gafitouser/frontend/ShowQRPage.kt
@@ -0,0 +1,80 @@
+package com.example.gafitouser.frontend
+
+import android.annotation.SuppressLint
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.gafitouser.user.component.BottomBar
+import com.example.gafitouser.user.component.PhotoProfile
+import com.example.gafitouser.user.component.QrCodeImage
+import com.example.gafitouser.user.component.TopBar
+import com.example.gafitouser.user.component.ui.theme.GafitoUserTheme
+
+
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ShowQrPage() {
+    Scaffold(
+        topBar = { TopBar() },
+        bottomBar = { BottomBar() }
+    ) {
+            paddingValues ->
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .padding(paddingValues)
+                .fillMaxSize()
+                .background(MaterialTheme.colorScheme.primary)
+//                .padding(bottom = 32.dp)
+
+        )  {
+            Box(
+                modifier = Modifier
+                    .padding(8.dp)
+                    .border(1.dp, Color.Black, shape = RoundedCornerShape(32.dp))
+                    .background(color = Color.White, shape = RoundedCornerShape(32.dp))
+            ) {
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(32.dp)
+                ) {
+                    PhotoProfile()
+//                    Spacer(modifier = Modifier.height(8.dp))
+                    QrCodeImage(content = "BA 1678 DA", size = 300.dp)
+
+                }
+            }
+        }
+    }
+}
+
+
+
+@Preview(showBackground = true)
+@Composable
+fun ShowQrPagePreview() {
+    GafitoUserTheme {
+        ShowQrPage()
+    }
+}
